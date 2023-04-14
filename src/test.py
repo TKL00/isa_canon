8279f7c35871629ad3fc72be63d36385184d7fd1
toleh20 <toleh20@student.sdu.dk>
2023-04-14 15:05:28 +0200

    Added python scrip that iterates through the cfi folder and tests canon on the graphs

--- src/test.py
+++ src/test.py
@@ -1,9 +1,22 @@
-def my_function(a):
-    for i in range(len(a)):
-        a[0] = "pear"
-        print(a[i])
+import networkx as nx
+from canonization import test_canon
+import os
 
-my_list = ["apple", "pear"]
+for (root, dirs, files) in os.walk("cfi"):
+    print(root, dirs, files)
 
-my_function(my_list)
-print(my_list)
+    for f in files:
+
+        with open(os.path.join(root, f), "r") as file:
+            print(f"Opening file {f}")
+            g = nx.Graph()
+            edges = []
+            file.readline()
+            for string in file:
+                temp = string[2:]
+                nodes = temp.split(" ")
+                g.add_edge(int(nodes[0]) - 1, int(nodes[1]) - 1)
+            
+            # print(g.edges)
+            if len(g.nodes) < 300:
+                print(test_canon(g))
